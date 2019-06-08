a3480c8028b7ba048f44bc893444faf0e73570cf
dimitarpg13 <dimitar_pg13@hotmail.com>
2019-06-08 09:06:45 -0400

    C++ template metaprogramming - p58

--- Metaprogramming/test_p58.cpp
+++ Metaprogramming/test_p58.cpp
@@ -51,6 +51,23 @@ operator-(quantity<T,D> x, quantity<T,D> y)
     return quantity<T,D>(x.value() - y.value());
 };
 
+struct plus_f
+{
+    template <class T1, class T2>
+    struct apply
+    {
+       typedef typename mpl::plus<T1,T2>::type type;
+    };
+};
+
+template <class T, class D1, class D2>
+quantity<T, typename mpl::transform<D1,D2,plus_f>::type>
+operator* (quantity<T,D1> x, quantity<T,D2> y)
+{
+    typedef typename mpl::transform<D1,D2,plus_f>::type dim;
+    return quantity<T,dim>( x.value() * y.value() );
+}
+
 template <class D1, class D2>
 struct divide_dimensions
   : mpl::transform< D1,D2,mpl::minus<_1,_2> > // forwarding again
@@ -70,6 +87,7 @@ int main(int argc, char **argv) {
     quantity<float,d_length> len1 (1.0f);
     quantity<float,d_length> len2(2.0f);
     len1 = len1 + len2;
-
+    quantity<float,d_acceleration> a(9.8f);
+    std::cout << "force = " << (m * a).value() << std::endl;
     return 0;
 }
